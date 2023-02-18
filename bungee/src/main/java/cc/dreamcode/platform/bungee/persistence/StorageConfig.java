07693610f15ae4c8056470ba19d81553d3f8a6a3
Ravis96 <ravis96@spoko.pl>
2023-02-18 11:30:01 +0100

    Improve storage-config

--- bungee/src/main/java/cc/dreamcode/platform/bungee/persistence/StorageConfig.java
+++ bungee/src/main/java/cc/dreamcode/platform/bungee/persistence/StorageConfig.java
@@ -5,6 +5,7 @@ import eu.okaeri.configs.annotation.Comment;
 import eu.okaeri.configs.annotation.NameModifier;
 import eu.okaeri.configs.annotation.NameStrategy;
 import eu.okaeri.configs.annotation.Names;
+import lombok.Setter;
 
 @Names(strategy = NameStrategy.HYPHEN_CASE, modifier = NameModifier.TO_LOWER_CASE)
 public class StorageConfig extends OkaeriConfig {
@@ -15,13 +16,13 @@ public class StorageConfig extends OkaeriConfig {
 
     @Comment({"Jaki prefix ustawic dla danych?",
             "Dla FLAT prefix nie jest uzywany."})
-    public String prefix = "dreamtemplate";
+    @Setter public String prefix = "dreamtemplate";
 
     @Comment("FLAT   : nie jest wymagane podawanie dodatkowych informacji")
-    @Comment("REDIS  : redis://localhost")
-    @Comment("MONGO  : mongodb://localhost:27017/?maxPoolSize=20&w=majority")
-    @Comment("MYSQL  : jdbc:mysql://localhost:3306/db?user=root&password=1234")
-    @Comment("H2     : jdbc:h2:file:./plugins/Dream-Template/storage;mode=mysql")
+    @Comment("REDIS  : redis://{username}:{password}@{host}:{port}")
+    @Comment("MONGO  : mongodb://{host}:{port}/?maxPoolSize=20&w=majority")
+    @Comment("MYSQL  : jdbc:mysql://{host}:{port}/{database}?user={username}&password={password}")
+    @Comment("H2     : jdbc:h2:file:./plugins/{data_folder}/storage;mode=mysql")
     public String uri = "";
 
 }
