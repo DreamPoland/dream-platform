11d4b5b1a9d4effe7e9edec37ab4a7babe1821b8
Ravis96 <ravis96@spoko.pl>
2023-06-26 13:10:12 +0200

    Remove text color from item-meta serdes

--- bukkit-config/src/main/java/cc/dreamcode/platform/bukkit/DreamBukkitConfig.java
+++ bukkit-config/src/main/java/cc/dreamcode/platform/bukkit/DreamBukkitConfig.java
@@ -1,11 +1,16 @@
 package cc.dreamcode.platform.bukkit;
 
+import cc.dreamcode.platform.bukkit.serializer.ItemMetaSerializer;
 import eu.okaeri.configs.serdes.OkaeriSerdesPack;
 import eu.okaeri.configs.yaml.bukkit.serdes.SerdesBukkit;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public interface DreamBukkitConfig {
 
     default OkaeriSerdesPack getConfigSerdesPack() {
-        return registry -> registry.register(new SerdesBukkit());
+        return registry -> {
+            registry.register(new SerdesBukkit());
+            registry.registerExclusive(ItemMeta.class, new ItemMetaSerializer());
+        };
     }
 }
---
+++ bukkit-config/src/main/java/cc/dreamcode/platform/bukkit/serializer/ItemMetaSerializer.java
@@ -0,0 +1,90 @@
+package cc.dreamcode.platform.bukkit.serializer;
+
+import eu.okaeri.configs.schema.GenericsDeclaration;
+import eu.okaeri.configs.serdes.DeserializationData;
+import eu.okaeri.configs.serdes.ObjectSerializer;
+import eu.okaeri.configs.serdes.SerializationData;
+import lombok.NonNull;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ItemMetaSerializer implements ObjectSerializer<ItemMeta> {
+
+    private static final char COLOR_CHAR = '\u00A7';
+    private static final char ALT_COLOR_CHAR = '&';
+
+    @Override
+    public boolean supports(@NonNull Class<? super ItemMeta> type) {
+        return ItemMeta.class.isAssignableFrom(type);
+    }
+
+    @Override
+    public void serialize(@NonNull ItemMeta itemMeta, @NonNull SerializationData data, @NonNull GenericsDeclaration generics) {
+
+        if (itemMeta.hasDisplayName()) {
+            data.add("display-name", this.decolor(itemMeta.getDisplayName()));
+        }
+
+        if (itemMeta.hasLore()) {
+            data.addCollection("lore", this.decolor(itemMeta.getLore()), String.class);
+        }
+
+        if (!itemMeta.getEnchants().isEmpty()) {
+            data.addAsMap("enchantments", itemMeta.getEnchants(), Enchantment.class, Integer.class);
+        }
+
+        if (!itemMeta.getItemFlags().isEmpty()) {
+            data.addCollection("item-flags", itemMeta.getItemFlags(), ItemFlag.class);
+        }
+    }
+
+    @Override
+    public ItemMeta deserialize(@NonNull DeserializationData data, @NonNull GenericsDeclaration generics) {
+
+        String displayName = data.get("display-name", String.class);
+
+        List<String> lore = data.containsKey("lore")
+                ? data.getAsList("lore", String.class)
+                : Collections.emptyList();
+
+        Map<Enchantment, Integer> enchantments = data.containsKey("enchantments")
+                ? data.getAsMap("enchantments", Enchantment.class, Integer.class)
+                : Collections.emptyMap();
+
+        List<ItemFlag> itemFlags = data.containsKey("item-flags")
+                ? data.getAsList("item-flags", ItemFlag.class)
+                : Collections.emptyList();
+
+        ItemMeta itemMeta = new ItemStack(Material.COBBLESTONE).getItemMeta();
+        if (itemMeta == null) {
+            throw new IllegalStateException("Cannot extract empty ItemMeta from COBBLESTONE");
+        }
+
+        if (displayName != null) {
+            itemMeta.setDisplayName(displayName);
+        }
+
+        itemMeta.setLore(lore);
+
+        enchantments.forEach((enchantment, level) -> itemMeta.addEnchant(enchantment, level, true));
+        itemMeta.addItemFlags(itemFlags.toArray(new ItemFlag[0]));
+
+        return itemMeta;
+    }
+
+    private List<String> decolor(List<String> text) {
+        return text.stream().map(this::decolor).collect(Collectors.toList());
+    }
+
+    private String decolor(String text) {
+        return text.replace(COLOR_CHAR + "", ALT_COLOR_CHAR + "");
+    }
+}
