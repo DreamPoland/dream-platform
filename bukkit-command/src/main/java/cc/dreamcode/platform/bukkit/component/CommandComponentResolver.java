8bd00c90af5e8e6c699ce188e84ddb6601a2d1fa
Ravis96 <ravis96@spoko.pl>
2023-10-16 22:10:17 +0200

    Bump dream-utilities to 1.2.17, rollback to dream-commands 1.4.14

---
+++ bukkit-command/src/main/java/cc/dreamcode/platform/bukkit/component/CommandComponentResolver.java
@@ -0,0 +1,46 @@
+package cc.dreamcode.platform.bukkit.component;
+
+import cc.dreamcode.command.bukkit.BukkitCommand;
+import cc.dreamcode.command.bukkit.BukkitCommandProvider;
+import cc.dreamcode.platform.component.ComponentClassResolver;
+import cc.dreamcode.utilities.builder.MapBuilder;
+import eu.okaeri.injector.Injector;
+import eu.okaeri.injector.annotation.Inject;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Map;
+
+@RequiredArgsConstructor
+public class CommandComponentResolver extends ComponentClassResolver<Class<BukkitCommand>> {
+
+    private @Inject BukkitCommandProvider bukkitCommandProvider;
+
+    @Override
+    public boolean isAssignableFrom(@NonNull Class<BukkitCommand> BukkitCommandClass) {
+        return BukkitCommand.class.isAssignableFrom(BukkitCommandClass);
+    }
+
+    @Override
+    public String getComponentName() {
+        return "command";
+    }
+
+    @Override
+    public Map<String, Object> getMetas(@NonNull Injector injector, @NonNull Class<BukkitCommand> bukkitCommandClass) {
+        final BukkitCommand bukkitCommand = injector.createInstance(bukkitCommandClass);
+
+        return new MapBuilder<String, Object>()
+                .put("name", bukkitCommand.getName())
+                .put("aliases", bukkitCommand.getAliases())
+                .build();
+    }
+
+    @Override
+    public Object resolve(@NonNull Injector injector, @NonNull Class<BukkitCommand> bukkitCommandClass) {
+        final BukkitCommand bukkitCommand = injector.createInstance(bukkitCommandClass);
+
+        this.bukkitCommandProvider.addCommand(bukkitCommand);
+        return bukkitCommand;
+    }
+}
