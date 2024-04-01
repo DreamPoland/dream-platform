8dfa9c9b59c78a80ca5abd5078a99a85fcc0ffd1
Ravis96 <ravis96@spoko.pl>
2024-04-01 11:41:09 +0200

    Release 1.10.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 allprojects {
     group = "cc.dreamcode.platform"
-    version = "1.10.7"
+    version = "1.10.8"
 
     apply(plugin = "java-library")
     apply(plugin = "maven-publish")
