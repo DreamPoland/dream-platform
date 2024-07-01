e1b8737dc0297e5ceb3dda510ff3a0e53b9e2268
Ravis96 <ravis96@spoko.pl>
2024-07-01 02:07:38 +0200

    Bump logback to 1.5.6

--- javacord/build.gradle.kts
+++ javacord/build.gradle.kts
@@ -6,8 +6,8 @@ dependencies {
     implementation("org.apache.logging.log4j:log4j-to-slf4j:2.17.0")
 
     // -- logger --
-    implementation("ch.qos.logback:logback-core:1.4.6")
-    implementation("ch.qos.logback:logback-classic:1.4.6")
+    implementation("ch.qos.logback:logback-core:1.5.6")
+    implementation("ch.qos.logback:logback-classic:1.5.6")
     implementation("org.slf4j:slf4j-api:2.0.13")
 
     // -- placeholders --
